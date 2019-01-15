779ba22daeaef23bcfa873c43d00c8aa11799d50
Anatoly38 <shameev38@gmail.com>
2019-01-15 23:45:59 +0000

    Модификация блейдов для загрузки по https - страница авторизации

--- resources/views/layout/authlayout.blade.php
+++ resources/views/layout/authlayout.blade.php
@@ -5,20 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ $title or  'Medinfo WebAdmin'}}</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/adminlte/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/plugins/iCheck/flat/blue.css') }}" rel="stylesheet" type="text/css" />
+    @if(config('medinfo.ssl_connection'))
+        <link href="{{ secure_asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ secure_asset('/adminlte/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ secure_asset('/plugins/iCheck/flat/blue.css') }}" rel="stylesheet" type="text/css" />
+    @else
 
+
+        <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/adminlte/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/plugins/iCheck/flat/blue.css') }}" rel="stylesheet" type="text/css" />
+    @endif
 </head>
 <body class="hold-transition register-page">
 @yield('content')
 
-<!-- jQuery 2.2.0 -->
-<script src="{{ asset('/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<!-- iCheck -->
-<script src="{{ asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
-
+@if(config('medinfo.ssl_connection'))
+    <script src="{{ secure_asset('/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
+    <script src="{{ secure_asset('/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ secure_asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
+@else
+    <script src="{{ asset('/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
+    <script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
+@endif
 </body>
 </html>
