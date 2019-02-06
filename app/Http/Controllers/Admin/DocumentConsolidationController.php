33391310cd28ae98938d457579d672a9310f1e21
Anatoly38 <shameev38@gmail.com>
2019-02-06 13:08:53 +0000

    Увеличено время выполнения скрипта консолидации до 10 минут

--- app/Http/Controllers/Admin/DocumentConsolidationController.php
+++ app/Http/Controllers/Admin/DocumentConsolidationController.php
@@ -68,7 +68,7 @@ class DocumentConsolidationController extends Controller
 
     public function consolidatePivoteTableByRuleAndUnitlist(Document $document, Table $table)
     {
-        set_time_limit(240);
+        set_time_limit(600);
         $aggregate = \App\Aggregate::find($document->id); // Вызываем модель, где хранятся свойства "сводного" документа
         $protected = is_null($aggregate) ? false : $aggregate->protected; // Свойство документа "защищен". True, если является защищенным перерасчета
         // Если документ защищен от свода - завершаем выполнение метода aggregateData с ошибкой
