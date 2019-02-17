a95438c1f82739a17135fd5869235578d9c8cc8f
Anatoly38 <shameev38@gmail.com>
2019-02-17 03:24:31 +0000

    Изменен (уменьшен) размер правого сплита при редактировании данных

--- resources/views/jqxdatainput/consolidatedashboard.blade.php
+++ resources/views/jqxdatainput/consolidatedashboard.blade.php
@@ -74,9 +74,9 @@ initTableMedstatExportButton();
 @push('loadjsscripts')
 @include('jqxdatainput.jsstack')
     @if(config('medinfo.ssl_connection'))
-        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=203') }}"></script>
+        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=204') }}"></script>
     @else
-        <script src="{{ asset('/medinfo/editdashboard.js?v=203') }}"></script>
+        <script src="{{ asset('/medinfo/editdashboard.js?v=204') }}"></script>
     @endif
 @endpush('loadjsscripts')
 
--- resources/views/jqxdatainput/formdashboard.blade.php
+++ resources/views/jqxdatainput/formdashboard.blade.php
@@ -62,9 +62,9 @@
 
 @push('loadjsscripts')
     @if(config('medinfo.ssl_connection'))
-        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=203') }}"></script>
+        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=204') }}"></script>
     @else
-        <script src="{{ asset('/medinfo/editdashboard.js?v=203') }}"></script>
+        <script src="{{ asset('/medinfo/editdashboard.js?v=204') }}"></script>
     @endif
 @endpush('loadjsscripts')
 
