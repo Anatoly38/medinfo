791606df3906d4eb94d94a92124812c819a446a4
Anatoly38 <shameev38@gmail.com>
2019-02-18 11:14:39 +0000

    Добавлена ссылка на открытие документа из протокола консолидации

--- resources/views/jqxdatainput/aggregatedashboard.blade.php
+++ resources/views/jqxdatainput/aggregatedashboard.blade.php
@@ -60,9 +60,9 @@
 
 @push('loadjsscripts')
     @if(config('medinfo.ssl_connection'))
-        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=205') }}"></script>
+        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=206') }}"></script>
     @else
-        <script src="{{ asset('/medinfo/editdashboard.js?v=205') }}"></script>
+        <script src="{{ asset('/medinfo/editdashboard.js?v=206') }}"></script>
     @endif
 @endpush('loadjsscripts')
 
--- resources/views/jqxdatainput/consolidatedashboard.blade.php
+++ resources/views/jqxdatainput/consolidatedashboard.blade.php
@@ -74,9 +74,9 @@ initTableMedstatExportButton();
 @push('loadjsscripts')
 @include('jqxdatainput.jsstack')
     @if(config('medinfo.ssl_connection'))
-        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=205') }}"></script>
+        <script src="{{ secure_asset('/medinfo/editdashboard.js?v=206') }}"></script>
     @else
-        <script src="{{ asset('/medinfo/editdashboard.js?v=205') }}"></script>
+        <script src="{{ asset('/medinfo/editdashboard.js?v=206') }}"></script>
     @endif
 @endpush('loadjsscripts')
 
