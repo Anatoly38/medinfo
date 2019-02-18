a7f57f9f7f7ff284a3bf96b70dcf2fd7cad4c5bd
Anatoly38 <shameev38@gmail.com>
2019-02-18 05:39:15 +0000

    Уменьшена ширина поля поиск строки

--- resources/views/jqxdatainput/formeditsplitter.blade.php
+++ resources/views/jqxdatainput/formeditsplitter.blade.php
@@ -76,7 +76,7 @@
                         @yield('tableConsolidateButton')
                         <form class="navbar-form navbar-right">
                             <div class="input-group" style="margin-right: 5px">
-                                <input type="text" class="form-control" id="SearchField" placeholder="Поиск строки">
+                                <input type="text" class="form-control" style="width: 130px" id="SearchField" placeholder="Поиск строки">
                                 <div class="input-group-btn">
                                     <button class="btn btn-default" id="ClearFilter" type="button">
                                         <i class="glyphicon glyphicon-remove"></i>
